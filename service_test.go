78fd462abfee6c1e8677e3fc26f6d7e0d29a3385
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-03 15:18:17 +0200

    Refactor service tests to include both valid and invalid config scenarios

--- service_test.go
+++ service_test.go
@@ -15,8 +15,22 @@ func TestConfigValidation(t *testing.T) {
 		assert.Nil(t, svc)
 		assert.Equal(t, "Config parameter is nil.", err.Error())
 	})
-	t.Run("valid config", func(t *testing.T) {
+	t.Run("invalid config", func(t *testing.T) {
 		cfg := &types.Config{}
+		svc, err := New(cfg)
+		assert.Error(t, err)
+		assert.Nil(t, svc)
+		assert.Equal(t, "Database configuration is invalid.", err.Error())
+		dErr, ok := errors.AsDetailedError(err)
+		assert.True(t, ok)
+		assert.NotNil(t, dErr)
+		fmt.Println(dErr.Cause())
+	})
+	t.Run("valid config", func(t *testing.T) {
+		cfg := &types.Config{
+			Driver: "postgres",
+			DSN:    "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable",
+		}
 		svc, err := New(cfg)
 		assert.Error(t, err)
 		assert.Nil(t, svc)
