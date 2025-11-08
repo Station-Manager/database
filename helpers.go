763dbcf0e479c530d79d0ebdd03271a306eb9f8a
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-08 15:27:41 +0200

    Remove unused Postgres helpers and `driverBasedCall` function for code cleanup and maintainability.

--- helpers.go
+++ helpers.go
@@ -17,15 +17,3 @@ func checkService(op errors.Op, s *Service) error {
 	}
 	return nil
 }
-
-func driverBasedCall(driver string, param any, sqliteCall func(any) (any, error), postgresCall func(any) (any, error)) (any, error) {
-	const op errors.Op = "database.Service.driverBasedCall"
-	switch driver {
-	case SqliteDriver:
-		return sqliteCall(param)
-	case PostgresDriver:
-		return postgresCall(param)
-	default:
-		return param, errors.New(op).Errorf("Unsupported database driver: %s", driver)
-	}
-}
--- postgres/helpers.go
+++
@@ -1,7 +0,0 @@
-package postgres
-
-import "github.com/Station-Manager/database/postgres/models"
-
-func GetQsoModel() models.Qso {
-	return models.Qso{}
-}
