ad70e895b5917610df4fdda006a87a73630622cf
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-09 15:32:32 +0200

    Add `DeleteQso` method for Postgres and SQLite databases
    - Implemented `DeleteQso` in `delete_qso.go` with support for both SQLite and Postgres.
    - Added service validation and transaction-based deletion logic for consistency.

---
+++ delete_qso.go
@@ -0,0 +1,109 @@
+package database
+
+import (
+	pgmodels "github.com/Station-Manager/database/postgres/models"
+	sqmodels "github.com/Station-Manager/database/sqlite/models"
+	"github.com/Station-Manager/errors"
+)
+
+func (s *Service) DeleteQso(id int64) error {
+	const op errors.Op = "database.Service.DeleteQso"
+
+	if err := checkService(op, s); err != nil {
+		return errors.New(op).Err(err)
+	}
+	if id < 1 {
+		return errors.New(op).Msg(errMsgInvalidId)
+	}
+
+	switch s.DatabaseConfig.Driver {
+	case SqliteDriver:
+		return s.sqliteDeleteQso(id)
+	case PostgresDriver:
+		return s.postgresDeleteQso(id)
+	default:
+		return errors.New(op).Errorf("Unsupported database driver: %s", s.DatabaseConfig.Driver)
+	}
+}
+
+func (s *Service) sqliteDeleteQso(id int64) error {
+	const op errors.Op = "database.Service.sqliteDeleteQso"
+
+	if err := checkService(op, s); err != nil {
+		return errors.New(op).Err(err)
+	}
+
+	//s.mu.RLock()
+	//h := s.handle
+	//isOpen := s.isOpen.Load()
+	//s.mu.RUnlock()
+
+	//if h == nil || !isOpen {
+	//	return errors.New(op).Msg(errMsgNotOpen)
+	//}
+
+	ctx, cancel := s.withDefaultTimeout(nil)
+	defer cancel()
+
+	tx, txCancel, err := s.BeginTxContext(ctx)
+	if err != nil {
+		return errors.New(op).Err(err)
+	}
+	defer txCancel()
+
+	model, err := sqmodels.FindQso(ctx, tx, id)
+	if err != nil {
+		return errors.New(op).Err(err)
+	}
+	if _, err = model.Delete(ctx, tx, false); err != nil {
+		_ = tx.Rollback()
+		return errors.New(op).Err(err)
+	}
+	if err = tx.Commit(); err != nil {
+		_ = tx.Rollback()
+		return errors.New(op).Err(err)
+	}
+
+	return nil
+}
+
+func (s *Service) postgresDeleteQso(id int64) error {
+	const op errors.Op = "database.Service.postgresDeleteQso"
+
+	if err := checkService(op, s); err != nil {
+		return errors.New(op).Err(err)
+	}
+
+	//s.mu.RLock()
+	//h := s.handle
+	//isOpen := s.isOpen.Load()
+	//s.mu.RUnlock()
+	//
+	//if h == nil || !isOpen {
+	//	return errors.New(op).Msg(errMsgNotOpen)
+	//}
+
+	ctx, cancel := s.withDefaultTimeout(nil)
+	defer cancel()
+
+	tx, txCancel, err := s.BeginTxContext(ctx)
+	if err != nil {
+		return errors.New(op).Err(err)
+	}
+	defer txCancel()
+
+	model, err := pgmodels.FindQso(ctx, tx, id)
+	if err != nil {
+		return errors.New(op).Err(err)
+	}
+	if _, err = model.Delete(ctx, tx); err != nil {
+		_ = tx.Rollback()
+		return errors.New(op).Err(err)
+	}
+	if err = tx.Commit(); err != nil {
+		_ = tx.Rollback()
+		return errors.New(op).Err(err)
+	}
+
+	return nil
+}
